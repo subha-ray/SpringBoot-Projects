ff28c478a8e60934618cfb880dfcd5e3082f07a2
subha-ray <subhasish34cse@gmail.com>
2025-06-25 13:09:12 +0530

    Fix: manually install Java 17 in build.sh for Render

--- JobAppSpring/build.sh
+++ JobAppSpring/build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-# Install Java 17 (Render default path)
-export JAVA_HOME=/opt/render/project/java17
+# Download and install Java 17 (Render doesn't provide it by default anymore)
+curl -s "https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz" -o java.tar.gz
+mkdir -p java
+tar -xzf java.tar.gz -C java --strip-components=1
+export JAVA_HOME=$PWD/java
 export PATH=$JAVA_HOME/bin:$PATH
 
-# Run Maven build
+# Verify Java is available
+java -version
+
+# Now run Maven build
 ./mvnw clean package
