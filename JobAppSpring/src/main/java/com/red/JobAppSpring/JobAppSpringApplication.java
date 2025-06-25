f298368a5267849bfe631f370fad681c1aa28681
subha-ray <subhasish34cse@gmail.com>
2025-06-25 14:05:02 +0530

    Final WAR deployment ready with JSP

--- JobAppSpring/src/main/java/com/red/JobAppSpring/JobAppSpringApplication.java
+++ JobAppSpring/src/main/java/com/red/JobAppSpring/JobAppSpringApplication.java
@@ -2,14 +2,19 @@ package com.red.JobAppSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-public class JobAppSpringApplication {
+public class JobAppSpringApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobAppSpringApplication.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JobAppSpringApplication.class);
+	}
 
 }
