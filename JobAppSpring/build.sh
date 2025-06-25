891ff2a7720d0b4313b467f9dd945884dd81a596
subha-ray <subhasish34cse@gmail.com>
2025-06-25 13:11:20 +0530

    Fix: use Adoptium JDK 17 download in build.sh

--- JobAppSpring/build.sh
+++ JobAppSpring/build.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-# Download and install Java 17 (Render doesn't provide it by default anymore)
-curl -s "https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz" -o java.tar.gz
+# Download and install OpenJDK 17 from Adoptium (no license required)
+curl -L https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.11+9/OpenJDK17U-jdk_x64_linux_hotspot_17.0.11_9.tar.gz -o java.tar.gz
+
 mkdir -p java
 tar -xzf java.tar.gz -C java --strip-components=1
 export JAVA_HOME=$PWD/java
 export PATH=$JAVA_HOME/bin:$PATH
 
-# Verify Java is available
+# Confirm Java installation
 java -version
 
-# Now run Maven build
+# Run Maven build
 ./mvnw clean package
